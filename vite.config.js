2b9dd1652720450585aec8d6d8845ccf3bad7a45
Rachit141 <rachitjain5265@gmail.com>
2025-07-05 10:31:46 +0530

    fix: configure port for Render

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
+  server: {
+    host: '0.0.0.0',
+    port: 10000,
+    strictPort: true, // Ensures Render won't try other ports
   },
-});
+  preview: {
+    port: 10000,
+    strictPort: true,
+  }
+})
